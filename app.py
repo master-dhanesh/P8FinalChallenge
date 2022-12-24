86cea88e23dc9ba5c6a5b2659a9f8c72c77cb470
master-dhanesh <dhanesh1296@gmail.com>
2022-12-24 17:22:39 +0530

    one

---
+++ app.py
@@ -0,0 +1,6 @@
+# import methods from Package.main and perform operation as instructed
+# run "CreateEmp()" for creating 2 Employees
+# run "UpdateEmp()" and update atleast one employee
+# run "ReadSingleEmp()" to read single employee
+# run "DeleteEmp()" to an employee
+# run "ReadAllEmp()" to read all the employees
